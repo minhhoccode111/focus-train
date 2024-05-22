aee5b328939283e5178a6fc2d5712622d5b31abb
minhhoccode111 <minhhoccode111@gmail.com>
2024-05-22 12:05:52 +0700

    feat: counter

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Focus Train
 
-remember that Huberman guy? Yes, he told us to pick a spot and stare at it for 2 minutes to train our focus
+Remember that Huberman guy?
+
+Yes, he told us to pick a spot and stare at it for 2 minutes to train our focus.
 
 So, enjoy (:
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import Button from "./fullscreen-button";
-import { useCallback, useState } from "react";
+import {
+  CSSProperties,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 
 function App() {
-  // some setting
-
-  const [isBorder, setIsBorder] = useState(false);
-
   const [isStarted, setIsStarted] = useState(false);
 
-  const [secondCount, setSecondCount] = useState(30);
+  const [secondCount, setSecondCount] = useState(10);
 
-  const [borderSize, setBorderSize] = useState(0);
+  const [borderSize, setBorderSize] = useState(2);
 
-  const [dotSize, setDotSize] = useState(1);
+  const [dotSize, setDotSize] = useState(2);
 
+  // time setting
   const incSecondCount = useCallback(
     function incTime() {
       setSecondCount((current) => current + 30);
     },
     [setSecondCount],
   );
-
   const decSecondCount = useCallback(
     function decTime() {
       setSecondCount((current) => (current > 30 ? current - 30 : current));
@@ -28,65 +30,92 @@ function App() {
     [setSecondCount],
   );
 
+  // dot size setting
   const incDotSize = useCallback(
     function () {
-      setDotSize((current) => (current < 20 ? ++current : current));
+      setDotSize((current) => (current < 40 ? current + 2 : current));
     },
     [setDotSize],
   );
-
   const decDotSize = useCallback(
     function () {
-      setDotSize((current) => (current > 1 ? --current : current));
+      setDotSize((current) => (current > 2 ? current - 2 : current));
     },
     [setDotSize],
   );
 
+  // border size setting
   const incBorderSize = useCallback(
     function () {
-      setBorderSize((current) => (current < 5 ? ++current : current));
+      setBorderSize((current) => (current < 10 ? current + 2 : current));
     },
     [setBorderSize],
   );
-
   const decBorderSize = useCallback(
     function () {
-      setBorderSize((current) => (current > 0 ? --current : current));
+      setBorderSize((current) => (current > 0 ? current - 2 : current));
     },
     [setBorderSize],
   );
 
-  const btn =
-    "border-white border rounded-xl text-white opacity-80 p-4 transition-all";
+  // constant style of all buttons
+  const btn = useMemo(
+    () =>
+      "border-white border rounded-md text-white opacity-80 px-4 py-2 transition-all",
+    [],
+  );
 
+  // dynamic styles on the fly based on settings
   const getDotSize = useCallback(
     function () {
       return dotSize + "px";
     },
     [dotSize],
   );
-
-  // TODO: add color pickers
-  // TODO: add moving dot
-  // TODO: add smooth moving algorithm for the dot
+  const getBorderSize = useCallback(
+    function () {
+      return borderSize + "px";
+    },
+    [borderSize],
+  );
 
   // because tailwind can't use dynamic variables to we have to set style
   // despite the fact that this is not best practice
-  const dotStyles = {
+  const dotStyles: CSSProperties = {
+    boxSizing: "content-box",
+    backgroundColor: "red", // TODO: color picker
+    borderColor: "white", // TODO: color picker
+    borderStyle: "solid",
+    borderWidth: getBorderSize(),
     height: getDotSize(),
     width: getDotSize(),
-
-    // TODO: add border size, border color, dot color
   };
 
+  // work with counter
+  useEffect(() => {
+    if (secondCount < 1) setIsStarted(false);
+  }, [secondCount]);
+
+  useEffect(() => {
+    if (isStarted && secondCount > 0) {
+      const interval = setInterval(() => {
+        setSecondCount((current) => current - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    } else {
+      setIsStarted(false);
+    }
+  }, [isStarted, secondCount]);
+
   return (
-    <div className="bg-gray-900 h-full flex flex-col gap-1" id="wrapper">
+    <div className="bg-gray-800 h-full flex flex-col gap-1" id="wrapper">
       <header className="flex gap-2 items-center justify-between">
         <div className="">
           <button
             type="button"
             onClick={() => setIsStarted(true)}
-            className={btn}
+            className={btn + " " + (isStarted ? "opacity-30" : "")}
           >
             start
           </button>
@@ -94,54 +123,56 @@ function App() {
           <button
             type="button"
             onClick={() => setIsStarted(false)}
-            className={btn}
+            className={btn + " " + (isStarted ? "" : "opacity-30")}
           >
             stop
           </button>
         </div>
 
         <div className="">
-          <button type="button" onClick={incBorderSize} className={btn}>
-            border ++
+          <button type="button" onClick={decBorderSize} className={btn}>
+            -2
           </button>
 
           <span className={btn}>border: {borderSize}</span>
 
-          <button type="button" onClick={decBorderSize} className={btn}>
-            border --
+          <button type="button" onClick={incBorderSize} className={btn}>
+            +2
           </button>
         </div>
 
         <div className="">
-          <button type="button" onClick={incSecondCount} className={btn}>
-            time ++
+          <button type="button" onClick={decSecondCount} className={btn}>
+            -30
           </button>
 
           <span className={btn}>time: {secondCount}</span>
 
-          <button type="button" onClick={decSecondCount} className={btn}>
-            time --
+          <button type="button" onClick={incSecondCount} className={btn}>
+            +30
           </button>
         </div>
 
         <div className="">
-          <button type="button" onClick={incDotSize} className={btn}>
-            dot size ++
+          <button type="button" onClick={decDotSize} className={btn}>
+            -2
           </button>
 
           <span className={btn}>dot size: {dotSize}</span>
 
-          <button type="button" onClick={decDotSize} className={btn}>
-            dot size --
+          <button type="button" onClick={incDotSize} className={btn}>
+            +2
           </button>
         </div>
       </header>
+
       <main className="flex-1 relative">
         <div
           style={dotStyles}
           className={`aspect-square rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`}
         ></div>
       </main>
+
       <footer className="text-center">
         <Button className={btn} />
       </footer>
